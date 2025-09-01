8c510fd1d4327d558bff53eac461e95a30fadd1d
MohamedHesham2024 <mohmedhesham2024@gmail.com>
2025-09-01 11:07:43 +0300

    edit-req-limit

--- utilts/rate-limit.js
+++ utilts/rate-limit.js
@@ -3,7 +3,7 @@ const { rateLimit }  = require('express-rate-limit')
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, 
-  limit: 1, 
+  limit: 100, 
   standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
   handler: (req, res) => {
